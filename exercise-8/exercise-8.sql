33ec494b176e5e30475eb90f8b6fa70c8c40e407
forestheims <forestheims@gmail.com>
2022-03-23 15:11:17 -0700

    exercise 8 complete

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,26 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT
+    name,
+    COUNT(rental_date),
+    RANK() OVER (ORDER BY COUNT(rental_date) DESC)
+FROM
+    rental
+LEFT JOIN
+    inventory
+ON
+    rental.inventory_id = inventory.inventory_id
+LEFT JOIN
+    film
+ON
+    inventory.film_id = film.film_id
+LEFT JOIN
+    film_category
+ON
+    film.film_id = film_category.film_id
+LEFT JOIN
+    category
+ON
+    category.category_id = film_category.category_id
+GROUP BY
+    category.name;
