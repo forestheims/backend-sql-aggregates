8d6d502ca4fa985573035b434f7f66e9f8c35be3
forestheims <forestheims@gmail.com>
2022-03-23 14:27:40 -0700

    exercise 4 complete

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,17 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+    category.name,
+    AVG(length)
+FROM
+    film
+LEFT JOIN
+    film_category
+ON
+    film.film_id = film_category.film_id
+LEFT JOIN
+    category
+ON
+    film_category.category_id = category.category_id
+GROUP BY
+    category.name;
