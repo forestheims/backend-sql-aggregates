0214c9f02818197dbefd0524747b3c812ec687a2
forestheims <forestheims@gmail.com>
2022-03-23 13:01:10 -0700

    exercise 3 completed

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,3 +1,30 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+SELECT
+    country,
+    AVG(amount)
+FROM
+    payment
+LEFT JOIN
+    customer
+ON
+    payment.customer_id = customer.customer_id
+LEFT JOIN
+    address
+ON
+    address.address_id = customer.address_id
+LEFT JOIN
+    city
+ON
+    city.city_id = address.city_id
+LEFT JOIN
+    country
+ON
+    country.country_id = city.country_id
+GROUP BY
+    country
+ORDER BY
+    avg DESC
+LIMIT
+    10;
